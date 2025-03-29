fdc9f13f5f92072eca33050d5f8ba14083e23cf8
DeepakPokhrel1 <deepakpokhrel0412@gmail.com>
2025-03-29 22:08:24 +0000

    Update Login.js

--- frontend/src/admin/pages/Login.js
+++ frontend/src/admin/pages/Login.js
@@ -16,7 +16,7 @@ const Login = () => {
     setIsLoading(true);
 
     try {
-      const response = await axios.post('http://localhost:8000/api/admin/login', {
+      const response = await axios.post('https://seraune1-backend.onrender.com/api/admin/login', {
         username,
         password
       });
